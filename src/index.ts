3179ddc8cb5ab90f5dcc7b3beb7a3afa7e8d329f
vino <vino0908@outlook.com>
2023-09-26 00:20:30 +0800

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,52 @@
 import { Elysia } from "elysia";
+import { jwt } from "@elysiajs/jwt";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(
+  jwt({
+    name: "jwt",
+    secret: "VINO_JWT",
+  }),
+);
+
+app.post("/password/login", async ({ body, jwt }) => {
+  if (!body) {
+    return {
+      code: 1,
+      message: "不合法的用户名或密码",
+      data: null,
+    };
+  }
+  const jwtStr = await jwt.sign(body as Record<string, string>);
+  return {
+    code: 0,
+    message: "登录成功",
+    data: {
+      accessToken: jwtStr,
+      refreshToken: jwtStr,
+      expiresIn: 3600,
+    },
+  };
+});
+
+app.get("/user", async ({ jwt, headers }) => {
+  try {
+    const jwtStr = headers.authorization?.split(" ")[1];
+    const userInfo = (await jwt.verify(jwtStr)) as { username: string };
+    return {
+      userId: "fake_user_id",
+      username: userInfo.username,
+    };
+  } catch (e) {
+    return {
+      code: 1,
+      message: "JWT 解析错误",
+      data: null,
+    };
+  }
+});
+
+app.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
